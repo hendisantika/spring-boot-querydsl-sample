e1d2fc0dd9c95fa98e9d783c0400cae8d60d1725
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 08:00:03 +0700

    Add dashboard3 in DashboardController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootquerydslsample/controller/DashboardController.java
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/controller/DashboardController.java
@@ -4,6 +4,9 @@ import id.my.hendisantika.springbootquerydslsample.entity.Team;
 import id.my.hendisantika.springbootquerydslsample.model.TeamResponse;
 import id.my.hendisantika.springbootquerydslsample.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,5 +44,14 @@ class DashboardController {
         return all.stream()
                 .map(team -> new TeamResponse(team.getId(), team.getName(), team.getMembers().size(), team.getMilestones().size()))
                 .toList();
+    }
+
+    @GetMapping("/v3/dashboard")
+    public Page<TeamResponse> dashboard3(Pageable pageable) {
+        Page<Team> all = teamRepository.findAll(pageable);
+        var content = all.stream()
+                .map(team -> new TeamResponse(team.getId(), team.getName(), team.getMembers().size(), team.getMilestones().size()))
+                .toList();
+        return new PageImpl<>(content, pageable, all.getTotalElements());
     }
 }
