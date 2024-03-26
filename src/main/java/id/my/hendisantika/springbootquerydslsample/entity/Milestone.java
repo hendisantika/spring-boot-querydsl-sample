82c95292379e4f169f3c39f309cbb0fd73540e48
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:48:30 +0700

    Add Milestone Entity 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootquerydslsample/entity/Milestone.java
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/entity/Milestone.java
@@ -1,5 +1,18 @@
 package id.my.hendisantika.springbootquerydslsample.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-querydsl-sample
@@ -10,5 +23,27 @@ package id.my.hendisantika.springbootquerydslsample.entity;
  * Time: 07:47
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "milestones")
 public class Milestone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDateTime celebratedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    @EqualsAndHashCode.Exclude
+    private Team team;
+
+    public Milestone(String name) {
+        this.name = name;
+        this.celebratedAt = LocalDateTime.now();
+    }
 }
