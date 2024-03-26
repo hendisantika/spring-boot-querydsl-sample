dc1d2fde8359deaf3bcaae518e07c7b5bc4abc5f
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:41:08 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/SpringBootQuerydslSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootquerydslsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerydslSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuerydslSampleApplication.class, args);
+    }
+
+}
