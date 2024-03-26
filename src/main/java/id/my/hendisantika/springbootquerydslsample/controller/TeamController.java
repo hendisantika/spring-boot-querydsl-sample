7641d75db01158ad62e96b5e22793a743c203cf2
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:58:28 +0700

    Add paginatedMembers in TeamController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootquerydslsample/controller/TeamController.java
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/controller/TeamController.java
@@ -3,6 +3,8 @@ package id.my.hendisantika.springbootquerydslsample.controller;
 import id.my.hendisantika.springbootquerydslsample.model.MemberResponse;
 import id.my.hendisantika.springbootquerydslsample.repository.DefaultTeamQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,5 +38,10 @@ class TeamController {
     @GetMapping("/v2/teams/{teamId}/members")
     public List<MemberResponse> searchMembers(@PathVariable long teamId, @RequestParam(required = false) String searchText) {
         return query.searchMembersByTeamId(teamId, searchText);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberResponse> paginatedMembers(Pageable pageable) {
+        return query.members(pageable);
     }
 }
