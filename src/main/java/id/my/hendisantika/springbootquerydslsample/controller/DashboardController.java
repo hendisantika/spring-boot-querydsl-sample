7611a3715cbb14bea283abd57be3c6e667147201
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:59:36 +0700

    Add dashboard1 in DashboardController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootquerydslsample/controller/DashboardController.java
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/controller/DashboardController.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.springbootquerydslsample.controller;
 
+import id.my.hendisantika.springbootquerydslsample.entity.Team;
+import id.my.hendisantika.springbootquerydslsample.model.TeamResponse;
 import id.my.hendisantika.springbootquerydslsample.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 class DashboardController {
 
     private final TeamRepository teamRepository;
+
+    @GetMapping("/v1/dashboard")
+    public List<TeamResponse> dashboard1() {
+        List<Team> all = teamRepository.findAllUsingJoinFetch();
+        return all.stream()
+                .map(team -> new TeamResponse(team.getId(), team.getName(), team.getMembers().size(), team.getMilestones().size()))
+                .toList();
+    }
 }
