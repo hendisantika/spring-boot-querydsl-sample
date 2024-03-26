c9b53f4d53087d7200265f88b9d1af434e4420ad
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:49:13 +0700

    Add findAllUsingJoinFetch in TeamRepository 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootquerydslsample/repository/TeamRepository.java
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/repository/TeamRepository.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.springbootquerydslsample.repository;
 
 import id.my.hendisantika.springbootquerydslsample.entity.Team;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +20,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * To change this template use File | Settings | File Templates.
  */
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    @Query("""
+            select distinct t
+                from Team t
+                left join fetch t.members
+            """)
+    List<Team> findAllUsingJoinFetch();
+
+    @EntityGraph(attributePaths = {
+            "members", "milestones"
+    })
+    List<Team> findAll();
+
+    @EntityGraph(attributePaths = {
+            "members", "milestones"
+    })
+    Page<Team> findAll(Pageable pageable);
 }
