0c6b14bc440ea358f68e8a7fe6a76920439f4cbf
hendisantika <hendisantika@yahoo.co.id>
2024-03-26 07:56:33 +0700

    Add QueryDslConfiguration 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootquerydslsample/config/QueryDslConfiguration.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.springbootquerydslsample.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-querydsl-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/26/24
+ * Time: 07:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+class QueryDslConfiguration {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
